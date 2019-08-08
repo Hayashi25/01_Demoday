203d960c049d21b54efba1fd792477109d260c5e
Mauricio Fagner <mauriciofagner2010@hotmail.com>
2019-08-08 15:48:46 -0300

    corrigir boas práticas do python em forms.py

--- website/forms.py
+++ website/forms.py
@@ -8,14 +8,37 @@ class CadastroEscola(forms.ModelForm):
         fields = ('nome_escola', 'endereco_escola', 'telefone_escola', 'email',
                   'rede_ensino', 'tipo_ensino', 'codigo_acesso', 'senha_acesso')
         widgets = {
-            'nome_escola': forms.TextInput(attrs={'required': True, 'id': 'nome_escola'}),
-            'endereco_escola': forms.TextInput(attrs={'required': True, 'id': 'endereco_escola'}),
-            'telefone_escola': forms.NumberInput(attrs={'required': True, 'id': 'telefone_escola'}),
-            'email': forms.EmailInput(attrs={'required': True, 'id': 'email'}),
-            'rede_ensino': forms.Select(choices=REDES_ENSINO, attrs={'required': True, 'id': 'rede_ensino'}),
-            'tipo_ensino': forms.Select(choices=TIPOS_ENSINO, attrs={'required': True, 'id': 'tipos_ensino'}),
-            'codigo_acesso': forms.TextInput(attrs={'required': True, 'id': 'codigo_acesso'}),
-            'senha_acesso': forms.PasswordInput(attrs={'required': True, 'id': 'senha_acesso'}),
+            'nome_escola': forms.TextInput(attrs={
+                'required': True,
+                'id': 'nome_escola'}),
+
+            'endereco_escola': forms.TextInput(attrs={
+                'required': True,
+                'id': 'endereco_escola'}),
+
+            'telefone_escola': forms.NumberInput(attrs={
+                'required': True,
+                'id': 'telefone_escola'}),
+
+            'email': forms.EmailInput(attrs={
+                'required': True,
+                'id': 'email'}),
+
+            'rede_ensino': forms.Select(choices=REDES_ENSINO, attrs={
+                'required': True,
+                'id': 'rede_ensino'}),
+
+            'tipo_ensino': forms.Select(choices=TIPOS_ENSINO, attrs={
+                'required': True,
+                'id': 'tipos_ensino'}),
+
+            'codigo_acesso': forms.TextInput(attrs={
+                'required': True,
+                'id': 'codigo_acesso'}),
+        
+            'senha_acesso': forms.PasswordInput(attrs={
+                'required': True, 
+                'id': 'senha_acesso'}),
         }
 
 class CadastrarParceiros(forms.ModelForm):
@@ -23,10 +46,24 @@ class CadastrarParceiros(forms.ModelForm):
         model = Parceiro
         fields = ('tipo_parceiro', 'nome_parceiro', 'email_parceiro', 'telefone_parceiro', 'mensagem_parceiro')
         widgets = {
-            'tipo_parceiro': forms.Select(choices=TIPO_PARCERIA, attrs={'required': True, 'id': 'tipo_parceiro'}),
-            'nome_parceiro': forms.TextInput(attrs={'required': True, 'id': 'nome_parceiro'}),
-            'email_parceiro': forms.EmailInput(attrs={'required': True, 'id': 'email_parceiro'}),
-            'telefone_parceiro': forms.NumberInput(attrs={'required': True, 'id': 'telefone_parceiro'}),
-            'mensagem_parceiro': forms.TextInput(attrs={'required': True, 'id': 'mensagem_parceiro'}),
+            'tipo_parceiro': forms.Select(choices=TIPO_PARCERIA, attrs={
+                'required': True,
+                'id': 'tipo_parceiro'}),
+
+            'nome_parceiro': forms.TextInput(attrs={
+                'required': True,
+                'id': 'nome_parceiro'}),
+
+            'email_parceiro': forms.EmailInput(attrs={
+                'required': True,
+                'id': 'email_parceiro'}),
+
+            'telefone_parceiro': forms.NumberInput(attrs={
+                'required': True,
+                'id': 'telefone_parceiro'}),
+
+            'mensagem_parceiro': forms.TextInput(attrs={
+                'required': True,
+                'id': 'mensagem_parceiro'}),
         }
 
