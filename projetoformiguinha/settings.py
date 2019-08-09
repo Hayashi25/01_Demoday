c89712fc3bbd0cdadaa32cfd588edf151fb7869c
Mauricio Fagner <mauriciofagner2010@hotmail.com>
2019-08-08 23:38:21 -0300

    desinstalar http cat

--- projetoformiguinha/settings.py
+++ projetoformiguinha/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'httpcat.middleware.HttpCatErrorHandler'
 ]
 
 ROOT_URLCONF = 'projetoformiguinha.urls'
