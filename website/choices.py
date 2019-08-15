77a0c7be64fff6d683026f6bf9caa437f29fcae1
Mauricio Martins <mauriciofagner2010@hotmail.com>
2019-08-15 15:14:29 -0300

    colocar opções de formulário do cadastro de aluno

--- website/choices.py
+++ website/choices.py
@@ -11,3 +11,18 @@ TIPOS_ENSINO = (
     ('Ensino Médio', 'Ensino Médio'),
     ('Ambos', 'Ambos')
 )
+
+TURMAS = (
+    ('5 SERIE', '5ª Série'),
+    ('6 SERIE', '6ª Série'),
+    ('7 SERIE', '7ª Série'),
+    ('8 SERIE', '8ª Série'),
+    ('1 ANO', '1º Ano'),
+    ('2 ANO', '2º Ano'),
+    ('3 ANO', '3º Ano')
+)
+
+GENEROS = (
+    ('Masculino', 'Masculino'),
+    ('Feminino', 'Feminino')
+)
