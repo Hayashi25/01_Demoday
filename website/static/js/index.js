4680896cfd097f841bb7f781910544be1c6cdd34
Gabriela <gabrielahpw@gmail.com>
2019-08-19 19:59:23 -0300

    atualizar js index 2, arrumar nome para funcionar img

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,5 +1,6 @@
+
 let imgs = [];
-let slideCaixa = document.getElementById("slide_caixa");
+let slideCaixa = document.getElementById("carrossel");
 let imgAtual;
 let numMaiorImg;
 let tempoImg;
@@ -7,39 +8,39 @@ let tempoImg;
 //------------------------------CARROSSEL----------------------------------------------------------
 
 //CRIAR UMA FUNÇÃO QUE FAZ O CARREGAMENTO DO CAMINHO PARA ACESSAR A IMAGEM E ADICIONAR AO VETOR
-function puxarImg(){
+function puxarImg() {
     let n = 1;
-    for( let i = 0; i<2; i++){
+    for (let i = 0; i < 3; i++) {
         imgs[i] = new Image();
-        imgs[i].src = "static/img/index/imagem"+n+".jpg";
+        imgs[i].src = "static/img/index/img" + n + ".jpg";
         n++;
     }
 }
 
 //APLICAR A IMAGEM NO BACKGROUND DA DIV, INFORMANDO QUAL IMAGEM VAI CARREGAR ATRAVÉS DO APARAMETRO
-function mostrarImagens(img){
-    slideCaixa.style.backgroundImage = "url('"+imgs[img].src+"')";
+function mostrarImagens(img) {
+    slideCaixa.style.backgroundImage = "url('" + imgs[img].src + "')";
 }
 
 //CHAMAR AS FUNCOES 
-function iniciar(){
+function iniciar() {
     puxarImg();
     imgAtual = 0;
-    numMaiorImg = imgs.length-1;
+    numMaiorImg = imgs.length - 1;
     mostrarImagens(imgAtual);
-    tempoImg=setInterval(troca, 4000);
+    tempoImg = setInterval(troca, 4000);
 }
 
 //TROCAR IMAGENS
-function troca(){
+function troca() {
     imgAtual++;
-    if(imgAtual > numMaiorImg){
+    if (imgAtual > numMaiorImg) {
         imgAtual = 0;
     }
     mostrarImagens(imgAtual);
 }
 
-//QUANDO A PAGINA FOR CARREGADA
+//QUANADO A PAGINA FOR CARREGADA
 window.addEventListener("load", iniciar);
 
 //------------------------------ FIM CARROSSEL----------------------------------------------------------
@@ -47,13 +48,70 @@ window.addEventListener("load", iniciar);
 //------------------------------MENU----------------------------------------------------------
 
 //TROCAR A COR DO MENU QUANDO A PAGINA ROLAR E SEU EIXO Y FOR MAIOR QUE 100PX
-window.onscroll = function (){
+window.onscroll = function () {
     let menu = document.querySelector("#menu");
-    if (window.pageYOffset > 100){
-        menu.style.backgroundColor = "#29402f";
+    if (window.pageYOffset > 100) {
+        menu.style.backgroundColor = "rgba(50, 112, 53, 0.900)";
     }
-    
-    else{
-        menu.style.backgroundColor = "rgba(41, 64, 47, 0.37)";
+    else {
+        menu.style.backgroundColor = "rgba(0, 0, 0, 0)";
     }
-}
+}// let imgs = [];
+// let slideCaixa = document.getElementById("slide_caixa");
+// let imgAtual;
+// let numMaiorImg;
+// let tempoImg;
+
+// //------------------------------CARROSSEL----------------------------------------------------------
+
+// //CRIAR UMA FUNÇÃO QUE FAZ O CARREGAMENTO DO CAMINHO PARA ACESSAR A IMAGEM E ADICIONAR AO VETOR
+// function puxarImg(){
+//     let n = 1;
+//     for( let i = 0; i<2; i++){
+//         imgs[i] = new Image();
+//         imgs[i].src = "static/img/index/imagem"+n+".jpg";
+//         n++;
+//     }
+// }
+
+// //APLICAR A IMAGEM NO BACKGROUND DA DIV, INFORMANDO QUAL IMAGEM VAI CARREGAR ATRAVÉS DO APARAMETRO
+// function mostrarImagens(img){
+//     slideCaixa.style.backgroundImage = "url('"+imgs[img].src+"')";
+// }
+
+// //CHAMAR AS FUNCOES 
+// function iniciar(){
+//     puxarImg();
+//     imgAtual = 0;
+//     numMaiorImg = imgs.length-1;
+//     mostrarImagens(imgAtual);
+//     tempoImg=setInterval(troca, 4000);
+// }
+
+// //TROCAR IMAGENS
+// function troca(){
+//     imgAtual++;
+//     if(imgAtual > numMaiorImg){
+//         imgAtual = 0;
+//     }
+//     mostrarImagens(imgAtual);
+// }
+
+// //QUANDO A PAGINA FOR CARREGADA
+// window.addEventListener("load", iniciar);
+
+// //------------------------------ FIM CARROSSEL----------------------------------------------------------
+
+// //------------------------------MENU----------------------------------------------------------
+
+// //TROCAR A COR DO MENU QUANDO A PAGINA ROLAR E SEU EIXO Y FOR MAIOR QUE 100PX
+// window.onscroll = function (){
+//     let menu = document.querySelector("#menu");
+//     if (window.pageYOffset > 100){
+//         menu.style.backgroundColor = "#29402f";
+//     }
+
+//     else{
+//         menu.style.backgroundColor = "rgba(41, 64, 47, 0.37)";
+//     }
+// }
