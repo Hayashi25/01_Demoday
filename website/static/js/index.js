7be4605fa81e0ba7aa8e728999d9f79e2d303f23
Mauricio Fagner <mauriciofagner2010@hotmail.com>
2019-08-20 03:05:06 -0300

    juntar nav e o carrossel atualizado

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,5 +1,5 @@
 let imgs = [];
-let slideCaixa = document.getElementById("slide_caixa");
+let slideCaixa = document.getElementById("carrossel");
 let imgAtual;
 let numMaiorImg;
 let tempoImg;
@@ -9,7 +9,7 @@ let tempoImg;
 //CRIAR UMA FUNÇÃO QUE FAZ O CARREGAMENTO DO CAMINHO PARA ACESSAR A IMAGEM E ADICIONAR AO VETOR
 function puxarImg(){
     let n = 1;
-    for( let i = 0; i<2; i++){
+    for(let i = 0; i<3; i++){
         imgs[i] = new Image();
         imgs[i].src = "static/img/index/imagem"+n+".jpg";
         n++;
@@ -39,21 +39,15 @@ function troca(){
     mostrarImagens(imgAtual);
 }
 
-//QUANDO A PAGINA FOR CARREGADA
+//QUANADO A PAGINA FOR CARREGADA
 window.addEventListener("load", iniciar);
 
-//------------------------------ FIM CARROSSEL----------------------------------------------------------
-
-//------------------------------MENU----------------------------------------------------------
-
-//TROCAR A COR DO MENU QUANDO A PAGINA ROLAR E SEU EIXO Y FOR MAIOR QUE 100PX
 window.onscroll = function (){
-    let menu = document.querySelector("#menu");
+    let menu = document.querySelector("#menu_login", "#menu_logout");
     if (window.pageYOffset > 100){
-        menu.style.backgroundColor = "#29402f";
+        menu.style.backgroundColor = "rgba(50, 112, 53, 0.900)";
     }
-    
     else{
-        menu.style.backgroundColor = "rgba(41, 64, 47, 0.37)";
+        menu.style.backgroundColor = "rgba(0, 0, 0, 0)";
     }
 }
